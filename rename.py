c86338997a2cde92e1b616750a04e359ac1ff3ef
Dinesh <dinumarish@gmail.com>
2022-11-01 10:05:21 +0100

    updated rename.py to count excess length correctly

--- rename.py
+++ rename.py
@@ -7,12 +7,12 @@ import sys
 def renameVideos(files,only_docfile=False):    
 
     excesslength = {
-                    range(1,15):'',
-                    range(15,31):'Ü1',
-                    range(31,70):'Ü2',
-                    range(71,121):'Ü3',
-                    range(121,500):'Ü4',
-                    range(500,50000):'Ü5'
+                    range(0,94):'',
+                    range(94,184):'Ü1',
+                    range(184,244):'Ü2',
+                    range(244,304):'Ü3',
+                    range(304,364):'Ü4',
+                    range(364,50000):'Ü5'
                    }
     lc = {}
     title = os.getcwd().split("\\")[-1]
@@ -23,7 +23,7 @@ def renameVideos(files,only_docfile=False):
         video = TinyTag.get(file)
         duration = floor(video.duration)
         duration = timedelta(seconds=duration)
-        category = duration.seconds
+        category = duration.seconds%60
         
         for k,v in excesslength.items():
             if category in k:
@@ -46,10 +46,16 @@ def renameVideos(files,only_docfile=False):
 
     with open('doc.txt','r') as fhand:
         content = fhand.read()
+    try:
+        lc['Ü4']+=lc['Ü5']
+        lc = sorted(lc.items())[1:]
+
+    except:
+        lc = sorted(lc.items())[1:]
 
     with open('doc.txt', 'w') as fhand:
         fhand.seek(0,0)
-        for item in sorted(lc.items())[1:]:
+        for item in lc:
             fhand.write(str(item).replace('(','').replace(')','').replace(',',':')+"\n")
         fhand.write(content)
 
@@ -66,6 +72,7 @@ def revertNames(files):
 
 arg = sys.argv
 files = list(filter(lambda x: x.endswith('mp4'),os.listdir()))
+files.sort(key=lambda x:int(x.split('_')[0]))
 
 if len(arg)==1:
     renameVideos(files,only_docfile=True)
@@ -73,13 +80,11 @@ if len(arg)==1:
 elif len(arg)==2 and arg[1].lower()=='p':
     renameVideos(files)
 
-elif len(arg)==2 and arg[1].lower()=='r':
+elif len(arg)==2 and arg[1].lower()=='u':
     revertNames(files)
 
 else:
     print("""
-    Das Skript akzeptiert nur 'p' oder 'r'
+    Das Skript akzeptiert nur 'p' oder 'u' zuzatlicherwiese
     'p' für die Umbenennung von mp4-Dateien gemäß der pppv-Konvention und 
-    'r' für die Umkehrung der pppv-Namen in ihre ursprünglichen Namen""")
-   
-
+    'u' für die Umkehrung der pppv-Namen in ihre ursprünglichen Namen""")
